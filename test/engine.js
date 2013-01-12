ee5ad319f88a69a344c88e77f5d4f147a086ee0d
ajlopez <webmaster@ajlopez.com>
2013-01-12 04:11:10 -0300

    engine.subjects(), .roles(), .contexts()

--- test/engine.js
+++ test/engine.js
@@ -6,3 +6,22 @@ var simplepermissions = require('..'),
 
 assert.ok(simplepermissions.createEngine);
 assert.ok(typeof simplepermissions.createEngine, 'function');
+
+// Create Engine
+
+var engine = simplepermissions.createEngine();
+
+assert.ok(engine);
+
+// It has subjects
+
+assert.ok(engine.subjects());
+
+// It has roles
+
+assert.ok(engine.roles());
+
+// It has contexts
+
+assert.ok(engine.contexts());
+
