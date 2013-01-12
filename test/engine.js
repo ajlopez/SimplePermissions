ed45a1acf8e4eb899ec0049550ebe1899043edbb
ajlopez <webmaster@ajlopez.com>
2013-01-12 04:07:08 -0300

    createEngine first test

---
+++ test/engine.js
@@ -0,0 +1,8 @@
+
+var simplepermissions = require('..'),
+    assert = require('assert');
+
+// createEngine is defined
+
+assert.ok(simplepermissions.createEngine);
+assert.ok(typeof simplepermissions.createEngine, 'function');
